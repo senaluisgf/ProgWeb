bc8c54945715e3b7ba5f83b8e46225821ff10e20
Luis "Shadow" Sena <sena.luisgf@gmail.com>
2023-09-19 22:02:00 -0400

    fix: js4

--- js/js4/script.js
+++ js/js4/script.js
@@ -2,10 +2,13 @@ function update() {
   const radius = parseFloat(document.radiusCalculate.radius.value);
   const areaElement = document.getElementById('area');
   const circumferenceElement = document.getElementById('circumference');
+  
   const { area, circumference} = calculate(radius)
+  
   console.log({radius, area, circumference})
-  areaElement.innerHTML = area.toFixed(2)
-  circumferenceElement.innerHTML = circumference.toFixed(2)
+  
+  areaElement.value = area.toFixed(2)
+  circumferenceElement.value = circumference.toFixed(2)
 }
 
 function calculate(radius) {
